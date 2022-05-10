0765fba5c6ebec68068deadab7361daca3331f95
Stein Arild Hoem <stein.hoem@artsdatabanken.no>
2022-05-10 22:41:11 +0200

    Progress on Clean Arch

---
+++ Nbic.References.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nbic.References.Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Nbic.References.EFCore;
+
+namespace Nbic.References.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ReferencesDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        protected Repository(ReferencesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public int Count()
+        {
+            return _dbSet.Count();
+        }
+
+        public Task<int> CountAsync()
+        {
+            return _dbSet.CountAsync();
+        }
+
+        public void Delete(object id)
+        {
+            var entity = Get(id);
+            if (entity != null)
+            {
+                if (_dbContext.Entry(entity).State == EntityState.Detached)
+                {
+                    _dbSet.Attach(entity);
+                }
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public T Get(object id)
+        {
+            var x = _dbSet.Find(id);
+            return x;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
