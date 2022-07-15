b0ecc55b59af74408b420ebb8baef6328c57c214
Stein A. Hoem <stein.hoem@gmail.com>
2022-07-15 15:58:26 +0200

    ok - wait a bit with this

--- Nbic.References/Startup.cs
+++ Nbic.References/Startup.cs
@@ -248,7 +248,7 @@ namespace Nbic.References
                     Console.WriteLine("Added FileDatabase");
                 }
 
-                services.AddDbContextPool<ReferencesDbContext>(options => options.UseSqlite(dbConnectionString));
+                services.AddDbContext<ReferencesDbContext>(options => options.UseSqlite(dbConnectionString));
                 Console.WriteLine("Added SqliteContext");
             }
         }
@@ -271,7 +271,7 @@ namespace Nbic.References
                 }
             }
 
-            services.AddDbContextPool<ReferencesDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<ReferencesDbContext>(options => options.UseSqlServer(connectionString));
             Console.WriteLine("Added SqlServerContext");
         }
 
