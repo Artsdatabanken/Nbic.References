ffe348558fc84b8a80f716cade39391c84bf1933
Stein A. Hoem <stein.hoem@artsdatabanken.no>
2024-03-25 11:09:54 +0100

    Remove some files

--- removed-deploy.sh
+++
@@ -1,4 +0,0 @@
-﻿#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push artsdatabanken/nbicreferences
-curl -X POST --data-urlencode "payload={\"channel\": \"$slack_chan\", \"username\": \"travis not the band\", \"text\": \"$slack_command\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$SLACK_TOKEN
